263175d8996b9976c61b5d6f866d80da37571c28
Vinicius Fogaca <viniciusfernandes38@gmail.com>
2022-01-11 17:25:57 -0300

    Add http interceptor

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,7 +1,30 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http/http.dart';
+import 'package:http_interceptor/http/interceptor_contract.dart';
+import 'package:http_interceptor/models/request_data.dart';
+import 'package:http_interceptor/models/response_data.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    print('Request');
+    print('url: ${data.url}');
+    print('header: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    print('Response');
+    print('status code: ${data.statusCode}');
+    print('header: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+}
 
 void findAll() async{
-  final Response response = await get(Uri.parse('http://192.168.11.7:8080/transactions'));
-  print(response.body);
-
+  final Client client = InterceptedClient.build(interceptors: [LoggingInterceptor()]);
+  final Response response = await client.get(Uri.parse('http://192.168.11.7:8080/transactions'));
 }
