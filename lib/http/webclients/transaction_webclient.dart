9d879fc4a7e3f33382b8fdb3faf57294fe36e52e
Vinicius Fogaca <viniciusfernandes38@gmail.com>
2022-01-12 14:50:11 -0300

    Using map to do some conversions on webclient

--- lib/http/webclients/transaction_webclient.dart
+++ lib/http/webclients/transaction_webclient.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert';
 
 import 'package:http/http.dart';
@@ -6,40 +5,26 @@ import 'package:lokibankv2/http/webclient.dart';
 import 'package:lokibankv2/models/transaction.dart';
 
 class TransactionWebClient {
-
   Future<List<Transaction>> findAll() async {
-    final Response response =
-    await client
-        .get(baseUrl)
-        .timeout(
-      const Duration(seconds: 5),
-    );
-    List<Transaction> transactions = _toTransactions(response);
-    return transactions;
+    final Response response = await client.get(baseUrl).timeout(
+          const Duration(seconds: 5),
+        );
+    final List<dynamic> decodeJson = jsonDecode(response.body);
+    return decodeJson
+        .map((dynamic json) => Transaction.fromJson(json))
+        .toList();
   }
 
   Future<Transaction> save(Transaction transaction) async {
     final String transactionJson = jsonEncode(transaction.toJson());
 
     final Response response = await client.post(baseUrl,
-        headers: {'Content-type': 'application/json', 'password': '1000',},
+        headers: {
+          'Content-type': 'application/json',
+          'password': '1000',
+        },
         body: transactionJson);
 
-    return _toTransaction(response);
-  }
-
-  List<Transaction> _toTransactions(Response response) {
-    final List<dynamic> decodeJson = jsonDecode(response.body);
-
-    final List<Transaction> transactions = [];
-    for (Map<String, dynamic> transactionJson in decodeJson) {
-      transactions.add(Transaction.fromJson(transactionJson ));
-    }
-    return transactions;
-  }
-
-  Transaction _toTransaction(Response response) {
-    Map<String, dynamic> json = jsonDecode(response.body);
-    return Transaction.fromJson(json);
+    return Transaction.fromJson(jsonDecode(response.body));
   }
 }
