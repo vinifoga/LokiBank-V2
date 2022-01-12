4d63d765443ab99fbd824c2d0ec9b2d0b7bf9417
Vinicius Fogaca <viniciusfernandes38@gmail.com>
2022-01-12 10:21:08 -0300

    Creating save on webclient

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -28,15 +28,18 @@ class LoggingInterceptor implements InterceptorContract {
   }
 }
 
+final Client client =
+InterceptedClient.build(interceptors: [LoggingInterceptor()]);
+
+Uri baseUrl = Uri.parse('http://192.168.11.7:8080/transactions');
+
 Future<List<Transaction>> findAll() async {
-  final Client client =
-      InterceptedClient.build(interceptors: [LoggingInterceptor()]);
   final Response response =
-      await client
-          .get(Uri.parse('http://192.168.11.7:8080/transactions'))
-          .timeout(
-            const Duration(seconds: 5),
-          );
+  await client
+      .get(baseUrl)
+      .timeout(
+    const Duration(seconds: 5),
+  );
   final List<dynamic> decodeJson = jsonDecode(response.body);
 
   final List<Transaction> transactions = [];
@@ -52,4 +55,29 @@ Future<List<Transaction>> findAll() async {
     transactions.add(transaction);
   }
   return transactions;
+}
+
+Future<Transaction> save(Transaction transaction) async {
+  final Map<String, dynamic> transactionMap = {
+    'value' : transaction.value,
+    'contact' : {
+      'name' : transaction.contact.name,
+      'accountNumber' : transaction.contact.accountNumber
+    }
+  };
+  final String transactionJson = jsonEncode(transactionMap);
+
+  final Response response = await client.post(baseUrl,
+      headers: {'Content-type': 'application/json', 'password': '1000',},
+      body: transactionJson);
+
+  Map<String, dynamic> json = jsonDecode(response.body);
+  final Map<String, dynamic> contactJson = json['contact'];
+  return Transaction(
+    json['value'],
+    Contact(
+      contactJson['name'],
+      contactJson['accountNumber'],
+    ),
+  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:lokibankv2/screens/dashboard.dart';
 
 import 'http/webclient.dart';
+import 'models/contact.dart';
+import 'models/transaction.dart';
 
 void main() {
   runApp(const Lokibankv2());
-  findAll().then((transactions) => print('new transactions $transactions'));
+  save(Transaction(157, Contact('Jorge', 1578))).then((transaction) => print(transaction));
 }
 
 class Lokibankv2 extends StatelessWidget {
